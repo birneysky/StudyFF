419fa3c389e722a5b0e02a023a5768a619c990f9
yingwei0831 <birneysky@163.com>
2018-06-09 20:05:33 +0800

    m

--- python/stockShare.py
+++ python/stockShare.py
@@ -1,4 +1,7 @@
 import tushare as ts
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot
 #获取某只股票历史交易数据
 '''
 date 日期
@@ -18,3 +21,10 @@ print(history_data)
 print(type(history_data))
 #获取a股所有股票的基本信息
 print(ts.get_stock_basics())
+
+fig = matplotlib.pyplot.gcf()
+df=ts.get_hist_data('600415',start='2015-04-01',end='2015-06-18')
+with pd.plotting.plot_params.use('x_compat', 'true'):
+    df.high.plot(color='r',figsize=(10,4),grid='on')
+    df.low.plot(color='b',figsize=(10,4),grid='on')
+    fig.savefig('./graph.png')
