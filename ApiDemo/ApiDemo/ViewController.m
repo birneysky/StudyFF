a7b91c77b41d11c386fd6cc3a889d2f16ea4f7f6
yingwei0831 <birneysky@163.com>
2018-07-19 00:02:24 +0800

    m

--- ApiDemo/ApiDemo/ViewController.m
+++ ApiDemo/ApiDemo/ViewController.m
@@ -40,11 +40,16 @@ static int interruptCallBack(void* arg) {
     avformat_open_input(&formatContext, path.UTF8String, NULL, NULL);
     avformat_find_stream_info(formatContext, NULL);
     
+    int videoStreamIndex = 0;
+    int audioStreamIndex = 0;
+    AVCodecContext* audioCodecCtx = NULL;
+    AVCodecContext* videoCodeCtx = NULL;
     /// 寻找音视频流:
     for (int i = 0; i < formatContext->nb_streams; i++) {
         AVStream* stream = formatContext->streams[i];
         if (AVMEDIA_TYPE_VIDEO == stream->codecpar->codec_type) {
             NSLog(@"videostreamIndex %d",i);
+            videoStreamIndex = i;
             AVCodecParameters* videoCodeParameter = stream->codecpar;
             AVCodec* codec = avcodec_find_decoder(videoCodeParameter->codec_id);
             if (!codec) {
@@ -52,14 +57,14 @@ static int interruptCallBack(void* arg) {
             } else {
                 AVCodecContext* codexCtx = avcodec_alloc_context3(codec);
                 avcodec_parameters_to_context(codexCtx, videoCodeParameter);
+                videoCodeCtx = codexCtx;
                 if (avcodec_open2(codexCtx, codec, NULL) < 0) {
                     NSLog(@"open video decoder failed");
-                } else {
-                    
                 }
             }
         } else if(AVMEDIA_TYPE_AUDIO == stream->codecpar->codec_type) {
             NSLog(@"audiostreamIndex %d",i);
+            audioStreamIndex = i;
             ///打开音频流解码器
             AVCodecParameters* audioCodeParameter = stream->codecpar;
             AVCodec* codec = avcodec_find_decoder(audioCodeParameter->codec_id);
@@ -68,12 +73,28 @@ static int interruptCallBack(void* arg) {
             } else {
                 AVCodecContext* codecCtx = avcodec_alloc_context3(codec);
                 avcodec_parameters_to_context(codecCtx, audioCodeParameter);
+                audioCodecCtx = codecCtx;
                 if(avcodec_open2(codecCtx, codec, NULL) < 0 ) {
                     NSLog(@"open audio decoder failed");
-                } else {
-                    
                 }
             }
+        }
+    }
+    /// 读取流内容，解码
+    AVPacket packet;
+    while (true) {
+        if (av_read_frame(formatContext, &packet) < 0) {
+            /// 小于0 出错或者读取完文件
+            break;
+        }
+        int packetStreamIndex = packet.stream_index;
+        if (packetStreamIndex == videoStreamIndex) {
+            AVFrame* frame = av_frame_alloc();
+            if(avcodec_receive_frame(videoCodeCtx, frame) == 0) {
+                
+            }
+        } else if(packetStreamIndex == audioStreamIndex) {
+            
         }
     }
 }
