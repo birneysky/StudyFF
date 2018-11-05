431acff84c013da0d5b97ff89346fc9dbb93d37f
yingwei0831 <birneysky@163.com>
2018-11-06 00:17:44 +0800

    m

--- av/src/av.cpp
+++ av/src/av.cpp
@@ -7,11 +7,17 @@ extern "C"{
 using namespace std;
 
 int main( int argc, char *argv[] ) {
+  av_log_set_level(AV_LOG_INFO);
+  if (argc < 2 ){
+    av_log(nullptr,AV_LOG_ERROR, "argc less than 2 \n");
+    return -1;
+  }
+  std::string fileName(argv[1]);  
   avformat_network_init();
-  av_log_set_level(AV_LOG_DEBUG);
 	av_log(NULL, AV_LOG_INFO, "%s\n", "Hello AV World");
-  file_test();
-  print_media_info("");
+  //file_test();
+  //print_media_info(fileName);
+  extra_audio_data(fileName);
   avformat_network_deinit();
 	 return 0;
 }
--- av/src/av_file.cpp
+++ av/src/av_file.cpp
@@ -33,3 +33,40 @@ int  print_media_info(const std::string& fileName) {
   avformat_close_input(&fmt_ctx);
   return 0;
 }
+
+int extra_audio_data(const std::string& fileName) {
+  /// av_init_packet() av_packet_unref()   初始化一个数据包结构体
+  /// av_find_best_stream()  在多媒体文件中找到最好的一路流
+  // av_read_frame  读取数据包
+  AVFormatContext* fmt_ctx = NULL;
+  int ret = avformat_open_input(&fmt_ctx, fileName.c_str(),NULL,NULL); 
+  if (ret < 0) {
+    av_log(NULL, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
+    return -1;
+  }
+  av_dump_format(fmt_ctx, 0, fileName.c_str(),0);
+  ret = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
+  if (ret < 0) {
+    av_log(NULL, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
+    avformat_close_input(&fmt_ctx);
+    return -1;
+  }
+  av_dump_format(fmt_ctx, 0, fileName.c_str(),0);
+  int audio_index = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
+  
+  if (audio_index < 0) {
+    av_log(NULL, AV_LOG_ERROR,"can't find the best stream %s\n",av_err2str(ret));
+    avformat_close_input(&fmt_ctx);
+    return -1;
+  }
+  AVPacket pkt;
+  av_init_packet(&pkt);
+  while(av_read_frame(fmt_ctx, &pkt) >= 0){
+    if(pkt.stream_index == audio_index) {
+      fwrite(pkt.data, 1, pkt.size, nullptr);
+    }
+  }
+
+  avformat_close_input(&fmt_ctx);
+  return 0;
+}
