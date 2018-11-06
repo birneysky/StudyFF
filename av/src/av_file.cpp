7bfaedc5e44ec69a8b85bc0884c1a19c56ac2414
yingwei0831 <birneysky@163.com>
2018-11-06 23:53:04 +0800

    m

--- av/src/av_file.cpp
+++ av/src/av_file.cpp
@@ -38,16 +38,17 @@ int extra_audio_data(const std::string& fileName) {
   /// av_init_packet() av_packet_unref()   初始化一个数据包结构体
   /// av_find_best_stream()  在多媒体文件中找到最好的一路流
   // av_read_frame  读取数据包
-  AVFormatContext* fmt_ctx = NULL;
-  int ret = avformat_open_input(&fmt_ctx, fileName.c_str(),NULL,NULL); 
+  AVFormatContext* fmt_ctx = nullptr;
+  int ret = avformat_open_input(&fmt_ctx, fileName.c_str(),nullptr,nullptr); 
   if (ret < 0) {
-    av_log(NULL, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
+    av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
     return -1;
   }
+  
   av_dump_format(fmt_ctx, 0, fileName.c_str(),0);
   ret = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
   if (ret < 0) {
-    av_log(NULL, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
+    av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
     avformat_close_input(&fmt_ctx);
     return -1;
   }
@@ -55,10 +56,12 @@ int extra_audio_data(const std::string& fileName) {
   int audio_index = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
   
   if (audio_index < 0) {
-    av_log(NULL, AV_LOG_ERROR,"can't find the best stream %s\n",av_err2str(ret));
+    av_log(nullptr, AV_LOG_ERROR,"can't find the best stream %s\n",av_err2str(ret));
     avformat_close_input(&fmt_ctx);
     return -1;
   }
+  AVStream* audio_stream = fmt_ctx->streams[audio_index];
+  av_log(nullptr,AV_LOG_INFO, "audio codec name: %s\n",avcodec_get_name(audio_stream->codec->codec_id));
   AVPacket pkt;
   av_init_packet(&pkt);
   while(av_read_frame(fmt_ctx, &pkt) >= 0){
