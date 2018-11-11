2ed8dc817f816c5a7631d3cdebc1e003eca8f638
yingwei0831 <birneysky@163.com>
2018-11-11 21:07:56 +0800

    m

--- av/src/av.cpp
+++ av/src/av.cpp
@@ -3,34 +3,134 @@
 #include "test/test.h"
 
 extern "C"{
-	#include <libavutil/log.h>
-  #include <libavformat/avformat.h>
+    #include <libavutil/log.h>
+    #include <SDL2/SDL.h>
+    #include <libavformat/avformat.h>
 }
 //using namespace std;
 
 int main( int argc, char *argv[] ) {
 
-  av_log_set_level(AV_LOG_INFO);
-  av_log(NULL, AV_LOG_INFO, "%s\n", "Hello AV World");
-  if (argc < 2 ){
-    av_log(nullptr,AV_LOG_ERROR, "argc less than 2 \n");
-    return -1;
-  }
-  std::string file_full_name(argv[1]);  
-  //avformat_network_init();
-  
-  //file_test();
-  //print_media_info(fileName);
-  extra_audio_data(file_full_name);
-  //avformat_network_deinit();
+    av_log_set_level(AV_LOG_INFO);
+    av_log(NULL, AV_LOG_INFO, "%s\n", "Hello AV World");
+    if (argc < 2 ){
+        av_log(nullptr,AV_LOG_ERROR, "argc less than 2 \n");
+        return -1;
+    }
+    std::string file_full_name(argv[1]);
+    //avformat_network_init();
 
-  std::shared_ptr<Test> p1 = std::make_shared<Test>();
-  std::cout <<  "1 ref:" << p1.use_count() << std::endl;
-  {
-    std::shared_ptr<Test> p2 = p1; 
-    std::cout << "2 ref:" << p1.use_count() <<std::endl;
-  }
-  std::cout << "3 ref:" << p1.use_count() << std::endl; 
-  p1->testFileStream();
-  return 0;
+    //file_test();
+    //print_media_info(fileName);
+    extra_audio_data(file_full_name);
+    //avformat_network_deinit();
+
+    std::shared_ptr<Test> p1 = std::make_shared<Test>();
+    std::cout <<  "1 ref:" << p1.use_count() << std::endl;
+    {
+        std::shared_ptr<Test> p2 = p1;
+        std::cout << "2 ref:" << p1.use_count() <<std::endl;
+    }
+    std::cout << "3 ref:" << p1.use_count() << std::endl;
+    p1->testFileStream();
+    
+    
+    /// 初始化SDL
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+    
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window",
+                                            200,
+                                            200,
+                                            640,
+                                            480,
+                                            SDL_WINDOW_SHOWN );
+    
+    if (!window) {
+        av_log(nullptr, AV_LOG_ERROR, "Faliled to create windows !");
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+    /// 创建渲染器 CreateRender
+    SDL_Renderer* render =  SDL_CreateRenderer(window, -1, 0);
+    //SDL_SetRenderDrawBlendMode(render, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
+    SDL_Rect window_rect = (SDL_Rect){0,0,640,480};
+    SDL_RenderDrawRect(render, &window_rect);
+    SDL_RenderClear(render);
+    SDL_RenderPresent(render);
+    /// 销毁渲染器
+    if (!render) {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+    /// 清空渲染器
+
+    /// sdl 所有的事件都存放在一个队列中
+    /// 所有对事件的操作，其实就是对队列的操作
+    ///  事件类型
+        /// SDL_WindowEvent :窗口事件
+        /// SDL_KeyboardEvent: 键盘事件
+        /// SDL_MouseMotionEvent: 鼠标事件
+    /// 事件的两种方式 SDL_PoolEvent 轮询  SDL_WaitEvent 等待触发，
+    ///SDL_Delay(30000);
+    bool quit = true;
+    SDL_Texture* texture = SDL_CreateTexture(render, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_TARGET, 640, 480);
+    SDL_Rect rect;
+    rect.w = 30;
+    rect.h = 30;
+    if (!texture) {
+        av_log(nullptr, AV_LOG_ERROR, "create texture failed \n");
+        SDL_DestroyWindow(window);
+        SDL_DestroyRenderer(render);
+        SDL_Quit();
+        return -1;
+    }
+    do {
+        SDL_Event event;
+        SDL_WaitEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                quit = false;
+                break;
+            default:
+                av_log(nullptr, AV_LOG_INFO, "event type is %d \n", event.type);
+        }
+
+//        rect.x = rand() % 600;
+//        rect.y = rand() % 450;
+//        av_log(nullptr, AV_LOG_INFO, "x %d, y %d \n",rect.x,rect.y);
+//        SDL_SetRenderTarget(render, texture);
+//        SDL_SetRenderDrawColor(render, 0, 0, 0, 0);
+//        SDL_RenderClear(render);
+//
+//        SDL_RenderDrawRect(render, &rect);
+//        SDL_SetRenderDrawColor(render, 255, 0, 0, 0);
+//        SDL_RenderFillRect(render, &rect);
+//        SDL_SetRenderTarget(render, NULL);
+//        SDL_RenderCopy(render, texture, NULL, NULL) ;
+//        SDL_RenderPresent(render);
+    } while (quit);
+    
+    /* 纹理渲染
+                       渲染器                 交换
+        内存图像 -------->  纹理 -------- > 窗口展示
+     
+        纹理相关api
+            SDL_CreateTexture()
+                 format:YUV,RGB
+                 access: Texture Target ,stream
+                 SDL_DestoryTexture()
+        渲染相关的api
+             SDL_SetRenderTarget() ///  设置渲染目标
+             SDL_RenderClear() /// 清屏，
+             SDL_RenderCopy ///  将纹理拷贝的显卡
+             SDL_RenderPresent() /// 将图形渲染显示
+     */
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(render);
+    SDL_Quit();
+
+    return 0;
 }
--- av/src/av_file.cpp
+++ av/src/av_file.cpp
@@ -9,32 +9,32 @@ extern "C" {
 }
 
 int file_test () {
-  int ret = avpriv_io_delete("./myteestfile.txt");
-  if (ret < 0) {
-    av_log(nullptr,AV_LOG_ERROR,"Failed to delete file mytestfile.txt\n");
-    return ret;
-  }
-   av_log(nullptr, AV_LOG_INFO, "Success to delete mytestfile.txt\n");
-  ret = avpriv_io_move("111.txt", "222.txt");
-  if (ret < 0) {
-    av_log(nullptr, AV_LOG_ERROR, "Failed to rename\n");
-    return ret;
-  }
-  av_log(nullptr, AV_LOG_INFO, "Success to rename\n");
-  return 0;
+    int ret = avpriv_io_delete("./myteestfile.txt");
+    if (ret < 0) {
+        av_log(nullptr,AV_LOG_ERROR,"Failed to delete file mytestfile.txt\n");
+        return ret;
+    }
+    av_log(nullptr, AV_LOG_INFO, "Success to delete mytestfile.txt\n");
+    ret = avpriv_io_move("111.txt", "222.txt");
+    if (ret < 0) {
+        av_log(nullptr, AV_LOG_ERROR, "Failed to rename\n");
+        return ret;
+    }
+    av_log(nullptr, AV_LOG_INFO, "Success to rename\n");
+    return 0;
 }
 
 int  print_media_info(const std::string& fileName) {
-  AVFormatContext* fmt_ctx = nullptr;
-  int ret = avformat_open_input(&fmt_ctx, fileName.c_str(),nullptr,nullptr); 
-  if (ret < 0) {
-    av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
-    return -1;
-  }
+    AVFormatContext* fmt_ctx = nullptr;
+    int ret = avformat_open_input(&fmt_ctx, fileName.c_str(),nullptr,nullptr);
+    if (ret < 0) {
+        av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
+        return -1;
+    }
 
-  av_dump_format(fmt_ctx, 0, fileName.c_str(),0);
-  avformat_close_input(&fmt_ctx);
-  return 0;
+    av_dump_format(fmt_ctx, 0, fileName.c_str(),0);
+    avformat_close_input(&fmt_ctx);
+    return 0;
 }
 
 
@@ -42,89 +42,89 @@ int extra_audio_data(const std::string& file_name) {
   /// av_init_packet() av_packet_unref()   初始化一个数据包结构体
   /// av_find_best_stream()  在多媒体文件中找到最好的一路流
   // av_read_frame  读取数据包
-  if (file_name.length() < 0) {
-    av_log(nullptr, AV_LOG_ERROR,"file_name is invalid");
-    return -1;
-  }
-  AVFormatContext* fmt_ctx = nullptr;
-  int ret = avformat_open_input(&fmt_ctx, file_name.c_str(),nullptr,nullptr); 
-  if (ret < 0) {
+    if (file_name.length() < 0) {
+        av_log(nullptr, AV_LOG_ERROR,"file_name is invalid");
+        return -1;
+    }
+    AVFormatContext* fmt_ctx = nullptr;
+    int ret = avformat_open_input(&fmt_ctx, file_name.c_str(),nullptr,nullptr);
+    if (ret < 0) {
     av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
-    return -1;
-  }
-  
-  av_dump_format(fmt_ctx, 0, file_name.c_str(),0);
-  ret = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
-  if (ret < 0) {
-    av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
-    avformat_close_input(&fmt_ctx);
-    return -1;
-  }
-  av_dump_format(fmt_ctx, 0, file_name.c_str(),0);
-  int audio_index = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
-  
-  if (audio_index < 0) {
-    av_log(nullptr, AV_LOG_ERROR,"can't find the best stream %s\n",av_err2str(ret));
-    avformat_close_input(&fmt_ctx);
-    return -1;
-  }
-  AVStream* audio_stream = fmt_ctx->streams[audio_index];
-  const char* codec_name = avcodec_get_name(audio_stream->codecpar->codec_id);
+        return -1;
+    }
 
-  std::string audio_file_name = std::string(file_name);
-  std::size_t pos = audio_file_name.find_last_of("/");
-  if (pos != std::string::npos) { 
-    audio_file_name = audio_file_name.substr(pos+1);
-  } 
-  pos = audio_file_name.find_last_of(".");
-  audio_file_name = audio_file_name.substr(0,pos+1) + codec_name;
+    av_dump_format(fmt_ctx, 0, file_name.c_str(),0);
+    ret = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
+    if (ret < 0) {
+        av_log(nullptr, AV_LOG_ERROR,"can't open file: %s\n",av_err2str(ret));
+        avformat_close_input(&fmt_ctx);
+        return -1;
+    }
+    av_dump_format(fmt_ctx, 0, file_name.c_str(),0);
+    int audio_index = av_find_best_stream(fmt_ctx,AVMEDIA_TYPE_AUDIO,-1,-1,nullptr,0);
 
-  av_log(nullptr,AV_LOG_INFO, "audio file name: %s\n",audio_file_name.c_str());
-  AVOutputFormat* output_fmt = av_guess_format(nullptr,audio_file_name.c_str(),nullptr);
-  if(!output_fmt) {
-    av_log(nullptr,AV_LOG_ERROR,"could not guss file format");
-    avformat_close_input(&fmt_ctx);
-    return -1;
-  }
-  AVFormatContext* ofmt_ctx = avformat_alloc_context();
-  ofmt_ctx->oformat = output_fmt;
-  AVStream* out_stream = avformat_new_stream(ofmt_ctx,nullptr);
- 
-  int err_code = avcodec_parameters_copy(out_stream->codecpar, audio_stream->codecpar);
-  if(err_code < 0) {
-    char errors[1024]; 
-    av_strerror(err_code, errors, 1024);
-     av_log(NULL, AV_LOG_ERROR, "Could not copy file parameters %s, %d(%s)\n",
+    if (audio_index < 0) {
+        av_log(nullptr, AV_LOG_ERROR,"can't find the best stream %s\n",av_err2str(ret));
+        avformat_close_input(&fmt_ctx);
+        return -1;
+    }
+    AVStream* audio_stream = fmt_ctx->streams[audio_index];
+    const char* codec_name = avcodec_get_name(audio_stream->codecpar->codec_id);
+
+    std::string audio_file_name = std::string(file_name);
+    std::size_t pos = audio_file_name.find_last_of("/");
+    if (pos != std::string::npos) {
+        audio_file_name = audio_file_name.substr(pos+1);
+    }
+    pos = audio_file_name.find_last_of(".");
+    audio_file_name = audio_file_name.substr(0,pos+1) + codec_name;
+
+    av_log(nullptr,AV_LOG_INFO, "audio file name: %s\n",audio_file_name.c_str());
+    AVOutputFormat* output_fmt = av_guess_format(nullptr,audio_file_name.c_str(),nullptr);
+    if(!output_fmt) {
+        av_log(nullptr,AV_LOG_ERROR,"could not guss file format");
+        avformat_close_input(&fmt_ctx);
+        return -1;
+    }
+    AVFormatContext* ofmt_ctx = avformat_alloc_context();
+    ofmt_ctx->oformat = output_fmt;
+    AVStream* out_stream = avformat_new_stream(ofmt_ctx,nullptr);
+
+    int err_code = avcodec_parameters_copy(out_stream->codecpar, audio_stream->codecpar);
+    if(err_code < 0) {
+        char errors[1024];
+        av_strerror(err_code, errors, 1024);
+        av_log(NULL, AV_LOG_ERROR, "Could not copy file parameters %s, %d(%s)\n",
                audio_file_name.c_str(),
                err_code,
                errors);
-  }
-
-  out_stream->codecpar->codec_tag = 0;
-  err_code = avio_open(&ofmt_ctx->pb, audio_file_name.c_str(), AVIO_FLAG_WRITE);
-  if(err_code < 0) {
-    
-  }
-
-  if(avformat_write_header(ofmt_ctx,nullptr) < 0 ) {
-    av_log(NULL, AV_LOG_DEBUG, "Error occurred when opening output file");
-    return -1;
-  }
-  AVPacket pkt;
-  av_init_packet(&pkt);
-  while(av_read_frame(fmt_ctx, &pkt) >= 0){
-    if(pkt.stream_index == audio_index) {
-      pkt.pts = av_rescale_q_rnd(pkt.pts, audio_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-      pkt.dts = pkt.pts;
-      pkt.duration = av_rescale_q(pkt.duration, audio_stream->time_base, out_stream->time_base);
-      pkt.pos = -1;
-      pkt.stream_index = 0;
-      av_interleaved_write_frame(ofmt_ctx, &pkt);
-      av_packet_unref(&pkt);
     }
-  }
 
-  av_write_trailer(ofmt_ctx); 
-  avformat_close_input(&fmt_ctx);
-  return 0;
+    out_stream->codecpar->codec_tag = 0;
+    err_code = avio_open(&ofmt_ctx->pb, audio_file_name.c_str(), AVIO_FLAG_WRITE);
+    if(err_code < 0) {
+
+    }
+
+    if(avformat_write_header(ofmt_ctx,nullptr) < 0 ) {
+        av_log(NULL, AV_LOG_DEBUG, "Error occurred when opening output file");
+        return -1;
+    }
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    while(av_read_frame(fmt_ctx, &pkt) >= 0){
+        if(pkt.stream_index == audio_index) {
+            pkt.pts = av_rescale_q_rnd(pkt.pts, audio_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
+            pkt.dts = pkt.pts;
+            pkt.duration = av_rescale_q(pkt.duration, audio_stream->time_base, out_stream->time_base);
+            pkt.pos = -1;
+            pkt.stream_index = 0;
+            av_interleaved_write_frame(ofmt_ctx, &pkt);
+            av_packet_unref(&pkt);
+        }
+    }
+
+    av_write_trailer(ofmt_ctx);
+    avformat_close_input(&fmt_ctx);
+    return 0;
 }
