b63df98155e2431ee6496c4e6a0b8bf411e761ad
赵炳东 <zhaobingdong@rongcloud.cn>
2018-11-19 18:06:11 +0800

    m

---
+++ av/playground/src/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  PlayGround
+//
+//  Created by birney on 2018/11/19.
+//  Copyright © 2018年 Grocery. All rights reserved.
+//
+
+#include <stdio.h>
+#include "test.h"
+
+int main(int argc, char* argv[]) {
+    Test t = Test();
+    t.testFileStream();
+    t.testSmartPoint();
+    return 0;
+}
